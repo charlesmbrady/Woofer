756f7343516afd4a08d9fb3aaf5e16e3206ecc77
Charles <charlesmbrady@gmail.com>
2019-04-06 09:49:25 -0400

    added cypresss tags

--- cypress/integration/woofer_spec.js
+++ cypress/integration/woofer_spec.js
@@ -5,10 +5,10 @@
 context('Woofer general test', () => {
 
   it('Should be able to register', () =>{
-    cy.visit('localhost:3333')
+    cy.visit('https://damp-waters-21948.herokuapp.com/')
 
     // Form is visible
-    cy.get('[data-test=needToLogin]').should('be.visible')
+    cy.get('[data-test=needToLogIn]').should('be.visible')
     cy.get('[data-test=register]').click()
     cy.get('[data-test=createYourAccount').should('be.visible').should('contain', 'Create Your Account')
 
@@ -19,17 +19,27 @@ context('Woofer general test', () => {
     cy.get('#inputPassword').type('trynapassword')
     cy.get('#add-user').click()
 
-    //should go back to homescreen
-    cy.get('#register-container').should('not.be.visible')
-    cy.get('#main-container').should('be.visible')
-    cy.get('[data-test=needToLogin]').should('not.be.visible')
+     //should go back to homescreen
+     cy.get('#main-container').should('be.visible')
+     cy.get('[data-test=needToLogin]').should('be.visible')
+     
+    // Can login
+    cy.get('#login-modal').click()
+    cy.get('#user-info').should('be.visible')
+    cy.get('#email').type('trynat@email.com')
+    cy.get('#user_password').type('trynapassword')
+    cy.get('#login').click()
+    cy.get('[data-test=home]').should('be.visible')
+    cy.get('[data-test=home]').click()
+    cy.get('#console-container').should('be.visible')
     cy.get('[data-test=username').should('contain', 'Tryna')
+    cy.get('[data-test=needToLogin]').should('be.visible')
+   
   })
 
   it('Should be able to logout', () => {
     cy.get('[data-test=logout]').click()
     cy.get('[data-test=needToLogin]').should('be.visible')
   })
-
   
 })
