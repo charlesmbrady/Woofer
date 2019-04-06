b82a9ad2062ab9a2664df1b546de58906167b527
Charles <charlesmbrady@gmail.com>
2019-04-06 00:17:47 -0400

    started adding data-test attributes for cypress

---
+++ cypress/integration/woofer_spec.js
@@ -0,0 +1,9 @@
+/// <reference types="Cypress" />
+
+context('Woofer general test', () => {
+    cy.visit('https://damp-waters-21948.herokuapp.com/')
+
+    it('Should be able to register', () =>{
+        cy.get('')
+    })
+})
